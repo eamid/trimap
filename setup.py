1d742a6b3792ef5f92688b844ce4bb2951bc7eca
Ehsan <ehsan.amid.ehsan@gmail.com>
2019-10-03 11:04:54 -0700

    bug fix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 configuration = {
     'name' : 'trimap',
-    'version': '1.0.7',
+    'version': '1.0.8',
     'description' : 'TriMap: Large-scale Dimensionality Reduction Using Triplets',
     'long_description' : readme(),
     'classifiers' : [
